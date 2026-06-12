91497a5d8d8d815b20d8cefd48b748be7f97fe66
Ayelen Acosta <ayelen.acca1@gmail.com>
2026-06-11 23:08:14 -0300

    Finaliza Sprint 3 con Express y EJS

--- app.js
+++ app.js
@@ -12,6 +12,35 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+app.get('/register', (req, res) => {
+    res.render('users/register');
+});
+
+app.get('/productDetail', (req, res) => {
+    res.render('products/productDetail');
+});
+
+app.get('/productCart', (req, res) => {
+    res.render('products/productCart');
+});
+
+app.get('/productCreate', (req, res) => {
+    res.render('products/productCreate');
+});
+
+app.get('/productEdit', (req, res) => {
+    res.render('products/productEdit');
+});
+
+app.use((req, res) => {
+    res.status(404).render('404');
+});
+
 app.listen(3000, () => {
     console.log('Servidor corriendo en puerto 3000');
 });
+
